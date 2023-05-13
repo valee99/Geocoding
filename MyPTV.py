21d53b5e3abf5d96a3a5bc6d62d7b0aa9b9f213e
valee99 <133514357+valee99@users.noreply.github.com>
2023-05-13 17:06:28 +0200

    MyPTV.py
    This code reads an Excel file containing clients' information such as ID, address, ecc. 
    The goal is to geocode clients' addresses and to create a new Excel file with desired info. In this Python code, the Excel output will contain their ID, address and geographical coordinates

---
+++ MyPTV.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import openpyxl
+import http.client, urllib.parse
+import json
+import requests
+from socket import *
+import urllib3
+
+
+#from the Excel File, read the columns named "id", "indirizzo", "cap", "paese", "provincia"
+df = pd.read_excel( r"FILE_PATH.xlsx",
+    usecols=["id", "indirizzo", "cap", "paese", "provincia"],
+)
+df["id"] = df["id"].astype(str)
+df["cap"] = df["cap"].astype(str)
+#delete blank spaces at the end and at the beginning 
+df = df.apply(lambda x: x.str.strip(), axis=1)
+# delete addresses with length <= 1
+df = df[df["indirizzo"].str.len() > 1]
+#delete blank spaces at the end and at the beginning
+df = df.apply(lambda x: x.str.strip(), axis=1)
+
+id = df["id"]
+Street = df["indirizzo"]
+Locality = df["paese"]
+Postal_Code = df["cap"]
+
+geos = []
+
+for id_el, Street_el, Locality_el, Postal_Code_el in zip(id, Street, Locality, Postal_Code):
+    
+    Country_Filter = "IT"
+    Country = "Italy"
+    indirizzo = Street_el + " " + Postal_Code_el + " " + str(Locality_el)
+
+    #here the country is ITALY
+    #if you need to use another country, just put the country name instead of Italy
+    url = "https://api.myptv.com/geocoding/v1/locations/by-address?country=Italy&locality=" + str(Locality_el) + "&postalCode=" + Postal_Code_el + "&street=" + Street_el + "&countryFilter=IT"
+    headers = {
+        'apiKey': "YOUR_MyPTV_API_KEY"
+    }
+    response = requests.request("GET", url, headers=headers)
+    
+    risposta = (response.text.encode('utf-8'))
+    #trasformation into a json type
+    a = risposta.decode('utf8').replace(" ' " , ' " ')
+    json_output = json.loads(a)
+    #print(json_output)
+    #print("")
+    try:
+        if len(json_output["locations"][0]) > 0: 
+
+            #latitudine = latitude & longitudine = longitude
+            latitudine = json_output["locations"][0]["referencePosition"]["latitude"]
+            longitudine = json_output["locations"][0]["referencePosition"]["longitude"]
+            geos.append([id_el , indirizzo, latitudine, longitudine])
+            print("TROVATO --> ", Street_el)
+    except KeyError as e:
+        print("errore di tipo ", e)
+        geos.append([id_el , indirizzo, "", ""])
+        print("NON TROVATO --> ", Street_el)
+    except gaierror:
+        geos.append([id_el , indirizzo, "", ""])
+        print("NON TROVATO --> ", Street_el)
+#    except (http.client.HTTPException, socket.time, socket.error):
+ #       geos.append([id_el , indirizzo, "", ""])
+ #   except requests.exceptions.ConnectionError:
+ #       print("")
+ #   except socket.gaierror:
+ #       print("")
+ #   except urllib3.exceptions.NewConnectionError:
+ #       print("")
+
+#convert the list into a dataframe
+dff = pd.DataFrame(
+    geos, columns=["ID Cliente", "Indirizzo", "Latitudine", "Longitudine"]
+)
+
+#write the dataframe into a new Excel File 
+dff.to_excel(r"FILE_PATH_+_EXCEL_NAME.xlsx",
+    sheet_name="SHEET_NAME",
+)
